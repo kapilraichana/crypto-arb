92d909f6515dd4f3a99c463f28e67da6221d644b
unknown <KR5021436@SYNSPZ4680.syntelorg.com>
2018-11-29 19:22:39 +0530

    Init, source code as-is except commented new-order request.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>	
+	
+</head>
+<body>
+	<h3 id="head_log1">Subscribe to orderbook</h3>
+	<div id="content_log1"></div>
+	<script src="script.js"></script>
+</body>
+</html>
